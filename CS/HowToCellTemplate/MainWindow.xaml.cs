adf1925e9fe92d7dd7863fe843dc2a2d98f21efd
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/HowToCellTemplate/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Data;
+using System.Data.OleDb;
+using System.Windows;
+using DevExpress.Xpf.PivotGrid;
+using HowToBindToMDB.NwindDataSetTableAdapters;
+using System.Windows.Markup;
+using System.Windows.Data;
+using System;
+using System.Windows.Media;
+
+namespace HowToBindToMDB {
+    public partial class MainWindow : Window {
+        NwindDataSet.SalesPersonDataTable salesPersonDataTable = new NwindDataSet.SalesPersonDataTable();
+        SalesPersonTableAdapter salesPersonDataAdapter = new SalesPersonTableAdapter();
+
+        public MainWindow() {
+            InitializeComponent();
+            pivotGridControl1.DataSource = salesPersonDataTable;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            salesPersonDataAdapter.Fill(salesPersonDataTable);
+        }
+    }
+
+    public class RoundConverter : MarkupExtension, IValueConverter {
+        #region IValueConverter Members
+        public object Convert(object value, Type targetType, object parameter, 
+							  System.Globalization.CultureInfo culture) {
+            return System.Convert.ToInt32(value);
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, 
+								  System.Globalization.CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            return this;
+        }
+    }
+}
---
+++ VB/HowToCellTemplate/MainWindow.xaml.vb
@@ -0,0 +1,52 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Data
+Imports System.Data.OleDb
+Imports System.Windows
+Imports DevExpress.Xpf.PivotGrid
+Imports HowToBindToMDB.NwindDataSetTableAdapters
+Imports System.Windows.Markup
+Imports System.Windows.Data
+Imports System
+Imports System.Windows.Media
+
+Namespace HowToBindToMDB
+    Partial Public Class MainWindow
+        Inherits Window
+        Private salesPersonDataTable As New NwindDataSet.SalesPersonDataTable()
+        Private salesPersonDataAdapter As New SalesPersonTableAdapter()
+
+        Public Sub New()
+            InitializeComponent()
+            pivotGridControl1.DataSource = salesPersonDataTable
+        End Sub 
+
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            salesPersonDataAdapter.Fill(salesPersonDataTable)
+        End Sub 
+    End Class 
+
+    Public Class RoundConverter
+        Inherits MarkupExtension
+        Implements IValueConverter
+        #Region "IValueConverter Members" 
+        Public Function Convert(ByVal value As Object, _
+								ByVal targetType As Type, _
+								ByVal parameter As Object, _
+								ByVal culture As System.Globalization.CultureInfo) _
+								As Object Implements IValueConverter.Convert
+            Return System.Convert.ToInt32(value)
+        End Function 
+        Public Function ConvertBack(ByVal value As Object, _
+								    ByVal targetType As Type, _
+									ByVal parameter As Object, _
+									ByVal culture As System.Globalization.CultureInfo) _
+									As Object Implements IValueConverter.ConvertBack
+            Throw New NotImplementedException()
+        End Function 
+        #End Region
+
+        Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object 
+            Return Me 
+        End Function 
+    End Class 
+End Namespace 
